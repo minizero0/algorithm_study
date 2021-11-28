5d0ba6e53f9762a1d9add5ba599951300c58b460
yeongmin <dudals1069@gmail.com>
2021-11-28 23:54:29 +0900

    11-28

--- algo_study(11-12)/algo7.c
+++ algo_study(11-12)/algo7.c
@@ -1,38 +1,37 @@
-#include <stdio.h>
-
-int	main(void)
-{
-const int MAX = 1000000;
-int og[MAX];
-int on ;
-bool c[MAX+1];
-int main()
-{
-    for(int i=2; i*i<=MAX; i++)
-    {
-        if(c[i] == false)
+    #include <stdio.h>
+    #include <stdbool.h>
+    
+    const int MAX = 1000000;
+    int og[MAX];
+    int on ;
+    bool c[MAX+1];    
+    int main()
+    {    
+        for(int i=2; i*i<=MAX; i++)
         {
-            og[on++]=i;
-        for(int j=i*i; j<=MAX; j+=i){
-            c[j] = true;
+            if(c[i] == false)
+            {
+                og[on++]=i;
+            for(int j=i*i; j<=MAX; j+=i){
+                c[j] = true;
+            }
+            }
         }
-        }
-    }
-    while(1)
-    {
-        int n;
-        scanf("%d",&n);
-        if(n<=4)
-            break;
-        for(int j=1; j<on; j++)
+        while(1)
         {
-            if(c[n-og[j]] == false){
-
-                    printf("%d = %d + %d\n",n,og[j],n-og[j]);
-                    break;
-                }
+            int n;
+            scanf("%d",&n);
+            if(n<=4)
+                break;
+            for(int j=1; j<on; j++)
+            {
+                if(c[n-og[j]] == false){
+                        
+                        printf("%d = %d + %d\n",n,og[j],n-og[j]);
+                        break;
+                    }
+            }
+            
         }
-
+        return 0;
     }
-    return 0;
-}
