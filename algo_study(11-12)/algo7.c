4e26d2c7a24ee52afe1db1076585016fb768f3e8
yeongmin <dudals1069@gmail.com>
2021-11-28 23:52:35 +0900

    11-28

--- algo_study(11-12)/algo7.c
+++ algo_study(11-12)/algo7.c
@@ -2,9 +2,37 @@
 
 int	main(void)
 {
-	int a,b,c,d;
-	scanf("%d\n", &a);
-	scanf("%d\n", &b);
-	scanf("%d\n", &c);
-	scanf("%d\n", &d);
+const int MAX = 1000000;
+int og[MAX];
+int on ;
+bool c[MAX+1];
+int main()
+{
+    for(int i=2; i*i<=MAX; i++)
+    {
+        if(c[i] == false)
+        {
+            og[on++]=i;
+        for(int j=i*i; j<=MAX; j+=i){
+            c[j] = true;
+        }
+        }
+    }
+    while(1)
+    {
+        int n;
+        scanf("%d",&n);
+        if(n<=4)
+            break;
+        for(int j=1; j<on; j++)
+        {
+            if(c[n-og[j]] == false){
+
+                    printf("%d = %d + %d\n",n,og[j],n-og[j]);
+                    break;
+                }
+        }
+
+    }
+    return 0;
 }
