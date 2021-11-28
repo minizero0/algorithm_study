eae0dc3a4d01915543ff0c509f923847a5bf6a3f
yeongmin <dudals1069@gmail.com>
2021-11-29 00:32:46 +0900

    11-28

--- algo_study(11-12)/algo7.c
+++ algo_study(11-12)/algo7.c
@@ -1,37 +1,37 @@
-    #include <stdio.h>
-    #include <stdbool.h>
-    
-    const int MAX = 1000000;
-    int og[MAX];
-    int on ;
-    bool c[MAX+1];    
-    int main()
-    {    
-        for(int i=2; i*i<=MAX; i++)
-        {
-            if(c[i] == false)
-            {
-                og[on++]=i;
-            for(int j=i*i; j<=MAX; j+=i){
-                c[j] = true;
-            }
-            }
-        }
-        while(1)
-        {
-            int n;
-            scanf("%d",&n);
-            if(n<=4)
-                break;
-            for(int j=1; j<on; j++)
-            {
-                if(c[n-og[j]] == false){
-                        
-                        printf("%d = %d + %d\n",n,og[j],n-og[j]);
-                        break;
-                    }
-            }
-            
-        }
-        return 0;
+#include <stdio.h>
+
+int prime[1000001];
+
+void checkprime()
+{
+    int i, j;
+ 
+    for (i = 2; i < 1000001; i++)     
+        prime[i] = 1;
+    for (i = 2; i * i < 1000001; i++) 
+    {
+        for (j = 2; prime[i] && i * j < 1000001; j++)
+            prime[i * j] = 0;
     }
+}
+ 
+int main()
+{
+    int n;
+    int i;
+ 
+    checkprime();
+ 
+    while (scanf("%d", &n) && n) {
+        for (i = 3; i * 2 <= n; i += 2) 
+        {
+            if (prime[i] && prime[n - i]) 
+            {
+                printf("%d = %d + %d\n", n, i, n - i);
+                break;
+            }
+        }
+    }
+ 
+    return 0;
+}
